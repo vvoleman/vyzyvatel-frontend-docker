d31ae2aa61c79d8486968c6b0aa2016e3d611110
maros-o <maros.mec@seznam.cz>
2023-02-09 23:20:42 +0100

    updateSocket

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState, useRef } from "react";
+import { createContext, useEffect, useState, useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { DEBUG } from "../constants";
