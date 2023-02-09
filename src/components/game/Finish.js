a133cad5e1a578421f86df7045bf1f4cb97bfd2c
maros-o <maros.mec@seznam.cz>
2023-02-09 01:29:11 +0100

    finish a href

--- src/components/game/Finish.js
+++ src/components/game/Finish.js
@@ -117,14 +117,14 @@ const Finish = () => {
           </div>
         </div>
         <div className="flex justify-center">
-          <motion.button
+          <a
             whileHover={{ scale: 1.05 }}
             type="submit"
             href="/"
             className="border-2 border-slate-400/40 sm:w-1/3 w-full text-slate-300 bg-slate-900/50 font-semibold rounded-lg text-lg py-2.5 text-center hover:bg-slate-700/80 shadow-xl shadow-black/40"
           >
             Zpět do Menu
-          </motion.button>
+          </a>
         </div>
       </div>
     </motion.div>
